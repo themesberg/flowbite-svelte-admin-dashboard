9a2367d7c765b41adbeefc126bb8782bb532fdc6
Shinichi Okada <okada.shin@gmail.com>
2023-01-24 09:50:21 +0900

    install tailwind, flowbite and dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+	</Alert>
+</div>
