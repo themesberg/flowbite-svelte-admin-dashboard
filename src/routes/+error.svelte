305510170a5d96573e1eaf3ef45d39b53ce30890
Jakub Jagielka <jjagielka@gmail.com>
2023-12-29 13:43:33 +0100

    initial layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import NotFound from '$lib/pages/NotFound.svelte';
+	import Maintenance from '$lib/pages/Maintenance.svelte';
+	import ServerError from '$lib/pages/ServerError.svelte';
+
+	const pages = {
+		400: Maintenance,
+		404: NotFound,
+		500: ServerError
+	};
+
+	const status = +$page.status;
+	const index = Object.keys(pages)
+		.map((x) => +x)
+		.reduce((p, c) => (p < status ? c : p));
+	const component = pages[index];
+</script>
+
+<svelte:component this={component}></svelte:component>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
 <script>
+	import { Avatar, DarkMode, NavBrand, NavHamburger, Navbar, Search } from 'flowbite-svelte';
 	import '../app.postcss';
-	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<div class="p-8">
-	<DarkMode />
+<Navbar fluid color="default" class="justify-between">
+	<div class="flex items-center justify-start w-fit">
+		<NavHamburger />
+		<NavBrand href="/" class="ms-2 md:me-24">
+			<img
+				src="/images/flowbite-svelte-icon-logo.svg"
+				class="me-3 h-6 sm:h-8"
+				alt="Flowbite Logo"
+			/>
+			<span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+				Flowbite
+			</span>
+		</NavBrand>
+		<Search size="sm" />
+	</div>
+
+	<div class="flex items-center ms-auto">
+		<DarkMode />
+		<Avatar size="sm" />
+	</div>
+</Navbar>
+
+<div class="lg:flex items-center justify-center">
 	<slot />
 </div>
