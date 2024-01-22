b120d2b2a206c92203976c07d8e36c2dc5e2609f
Jakub Jagielka <jjagielka@gmail.com>
2024-01-22 23:51:03 +0100

    pricing page start

---
+++ src/routes/(no-sidebar)/pages/pricing/PriceCardListItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Li } from 'flowbite-svelte';
+	import { CheckOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Li icon class="space-x-3">
+	<slot name="icon">
+		<CheckOutline size="sm" class="ms-1 text-green-500 dark:text-green-400" />
+	</slot>
+	<span><slot /></span>
+</Li>
---
+++ src/routes/(no-sidebar)/pages/pricing/PriceCardPrice.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let value: string;
+	export let period: string = '';
+</script>
+
+<div class="flex items-baseline my-8">
+	<span class="mr-2 text-5xl font-extrabold">{value}</span>
+	{#if period}
+		<span class="text-gray-500 dark:text-gray-400">/{period}</span>
+	{/if}
+</div>
