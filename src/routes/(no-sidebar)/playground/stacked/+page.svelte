1b84e51cd8cbbc3af91355a7e8a0c8c94bbe3241
Jakub Jagielka <jjagielka@gmail.com>
2024-02-12 22:44:22 +0100

    playground tuning

--- src/routes/(no-sidebar)/playground/stacked/+page.svelte
+++ src/routes/(no-sidebar)/playground/stacked/+page.svelte
@@ -1,15 +1,6 @@
 <script>
-	import {
-		Breadcrumb,
-		BreadcrumbItem,
-		Heading,
-		Footer,
-		FooterLinkGroup,
-		FooterLink,
-		FooterCopyright
-	} from 'flowbite-svelte';
+	import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
 	import Navbar from '../../../(sidebar)/Navbar.svelte';
-	import EmptyCard from './EmptyCard.svelte';
 	import Playground from './Playground.svelte';
 </script>
 
@@ -23,7 +14,7 @@
 	class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900"
 >
 	<Playground />
-	<Footer class="flex justify-between text-sm">
+	<Footer class="flex justify-between px-4 py-6 text-sm md:py-10 xl:px-0">
 		<FooterCopyright href="/" by="2023 Flowbite.com" year={2019} spanClass="text-gray-500" />
 		<FooterLinkGroup
 			ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
