64f99e6a19bf9c47a899b8ed40b1b62776794569
Jakub Jagielka <jjagielka@gmail.com>
2024-03-10 07:27:26 +0100

    main logo font color

--- src/routes/(sidebar)/Navbar.svelte
+++ src/routes/(sidebar)/Navbar.svelte
@@ -19,14 +19,15 @@
 	export let drawerHidden = false;
 </script>
 
-<Navbar {fluid} class="mt-px" color="default">
+<Navbar {fluid} class="mt-px text-black" color="default">
 	<NavHamburger
 		onClick={() => (drawerHidden = !drawerHidden)}
 		class="m-0 me-3 md:block lg:hidden"
 	/>
 	<NavBrand href="/" class="py-1.5 lg:w-60">
 		<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl"
+			>Flowbite</span
 		>
 	</NavBrand>
 	<div class="hidden lg:block lg:ps-4">
