3a50f2ba6d401ab4232099ceafa2bf927b1c14d0
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-17 12:53:13 +0100

    fix: remove vercel/analytics inject()

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
 	import modeobserver from '$lib/modeobserver';
 	import { onMount } from 'svelte';
-	import { inject } from '@vercel/analytics';
-	inject()
 
 	onMount(modeobserver);
 </script>
