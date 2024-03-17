cd9a89b725b8b3182d40fe40fc12914025eaf662
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-17 07:49:40 +0100

    feat: add @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import modeobserver from '$lib/modeobserver';
 	import { onMount } from 'svelte';
+	import { inject } from '@vercel/analytics';
+	inject()
 
 	onMount(modeobserver);
 </script>
