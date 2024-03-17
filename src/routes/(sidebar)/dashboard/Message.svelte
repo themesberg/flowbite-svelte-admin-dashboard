50749435fa3df8489a2e8f5f06c62738b0f58db4
Jakub Jagielka <jjagielka@gmail.com>
2024-03-17 07:12:14 +0100

    textarea in dashboard

--- src/routes/(sidebar)/dashboard/Message.svelte
+++ src/routes/(sidebar)/dashboard/Message.svelte
@@ -1,24 +1,29 @@
 <script>
 	import { Button, Textarea, Toolbar, ToolbarButton } from 'flowbite-svelte';
 	import { ImageSolid, MapPinAltSolid, PaperClipSolid } from 'flowbite-svelte-icons';
+	import { setContext } from 'svelte';
+
+	setContext('background', false);
 </script>
 
-<Textarea rows="8" placeholder="Write your message" required>
-	<div slot="footer" class="flex items-center justify-between">
-		<Button type="submit" size="xs">Post comment</Button>
-		<Toolbar embedded class="text-gray-500 dark:text-gray-400">
-			<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
-				<PaperClipSolid size="md" />
-				<span class="sr-only">Attach file</span>
-			</ToolbarButton>
-			<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
-				<MapPinAltSolid size="md" />
-				<span class="sr-only">Set location</span>
-			</ToolbarButton>
-			<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
-				<ImageSolid size="md" />
-				<span class="sr-only">Upload image</span>
-			</ToolbarButton>
-		</Toolbar>
-	</div>
-</Textarea>
+<form>
+	<Textarea rows="8" placeholder="Write your message" required>
+		<div slot="footer" class="flex items-center justify-between">
+			<Button type="submit" size="xs">Post comment</Button>
+			<Toolbar embedded class="text-gray-500 dark:text-gray-400" slot="foot">
+				<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
+					<PaperClipSolid size="md" />
+					<span class="sr-only">Attach file</span>
+				</ToolbarButton>
+				<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
+					<MapPinAltSolid size="md" />
+					<span class="sr-only">Set location</span>
+				</ToolbarButton>
+				<ToolbarButton class="p-2 hover:text-gray-900 dark:hover:text-white">
+					<ImageSolid size="md" />
+					<span class="sr-only">Upload image</span>
+				</ToolbarButton>
+			</Toolbar>
+		</div>
+	</Textarea>
+</form>
