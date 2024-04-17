aacf7d76c23fd1ad4153200fa67d8b882f4f092c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-17 22:15:31 +0200

    fix: update google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	import modeobserver from '$lib/modeobserver';
 	import { onMount } from 'svelte';
+	import Runatics from './utils/Runatics.svelte';
+  export let data;
+  const analyticsId = data.ANALYTICS_ID
 
 	onMount(modeobserver);
 </script>
-
+<Runatics {analyticsId} />
 <slot />
