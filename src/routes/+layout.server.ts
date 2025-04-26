45b311682786557b166ff75d98c11ca6f4819922
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-04-26 11:03:55 +0200

    fix: ANALYTICS_ID

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { LayoutServerLoad } from './$types';
-import { ANALYTICS_ID_FLOWBITE } from '$env/static/private';
+import { ANALYTICS_ID } from '$env/static/private';
 const json = (r: Response) => r.json();
 // export const prerender = true;
 
@@ -7,7 +7,7 @@ export const load: LayoutServerLoad = async ({ fetch }) => {
   try {
     const posts = await fetch('/api/posts').then(json);
     // console.log('posts: ', posts);
-    return { ANALYTICS_ID_FLOWBITE, posts };
+    return { ANALYTICS_ID, posts };
   } catch (error) {
     console.error(`Error in load function for /: ${error}`);
   }
