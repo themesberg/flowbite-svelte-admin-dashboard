66453e3b765f8dd1cd1b31ff09ae43e46b522d4f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-04-27 10:07:56 +0200

    docs: about page update

--- src/routes/(sidebar)/about/+page.svelte
+++ src/routes/(sidebar)/about/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Breadcrumb, BreadcrumbItem, Heading, List, Li } from 'flowbite-svelte';
   import { CheckCircleSolid } from 'flowbite-svelte-icons';
+  import { HighlightCompo } from '../../utils'
   const name = __NAME__;
   const admindashboardVersion = __VERSION__;
   const svelteVersion = __SVELTEVERSION__;
@@ -10,10 +11,16 @@
   const flowbitesvelteVersion = __FLOWBITESVELTE__;
   const flowbitesvelteiconsVersion = __FLOWBITESVETEICONS__;
   const tailwindmergeVersion = __TAILWINDMERGE__;
+
+  const modules = import.meta.glob('./md/*.md', {
+		query: '?raw',
+		import: 'default',
+		eager: true
+	});
 </script>
 
-<main class="relative h-screen w-full overflow-y-auto bg-white dark:bg-gray-800">
-  <div class="p-8">
+<main class="h-screen w-full overflow-y-auto bg-white dark:bg-gray-800">
+  <div class="p-12">
     <Breadcrumb class="mb-5">
       <BreadcrumbItem home>Home</BreadcrumbItem>
       <BreadcrumbItem>About</BreadcrumbItem>
@@ -49,5 +56,7 @@
         tailwind-merge: {tailwindmergeVersion}
       </Li>
     </List>
+    <Heading tag="h2" class="my-8 text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Installation</Heading>
+    <HighlightCompo class="mb-8" codeLang="js" code={modules['./md/installation.md'] as string} />
   </div>
 </main>
