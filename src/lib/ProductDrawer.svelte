2da73aa55b590e71ea3c66ba5fbd2027c33afd58
Johnson <johnsonzhang0311@126.com>
2025-09-15 15:02:20 +0800

    fix:Sidebar,CRUD product when edit the drawer display original data

--- src/lib/ProductDrawer.svelte
+++ src/lib/ProductDrawer.svelte
@@ -3,17 +3,32 @@
   import { CloseOutline } from 'flowbite-svelte-icons';
   import type { ProductDrawerProps } from './types';
 
-  let { hidden = $bindable(true), title = 'Add new product' }: ProductDrawerProps = $props();
+  let { hidden = $bindable(true), title = 'Add new product', data = {} }: ProductDrawerProps = $props();
+
+  function init(form: HTMLFormElement) {
+    if (data?.title) [data.title, data.price] = data.title.split(' ');
+    for (const key in data) {
+      // console.log(key, data[key]);
+      const el = form.elements.namedItem(key);
+      if (el) {
+        if (el instanceof HTMLInputElement) {
+          el.value = data[key];
+        } else if (el instanceof HTMLTextAreaElement) {
+          el.value = data[key];
+        }
+      }
+    }
+  }
 </script>
 
-<Heading tag="h5" class="mb-6 text-sm font-semibold uppercase">{title}</Heading>
+<Heading tag="h5" class="mb-6 text-sm font-semibold uppercase">{Object.keys(data).length ? 'Edit product' : 'Add new product'}</Heading>
 <CloseButton onclick={() => (hidden = true)} class="absolute top-2.5 right-2.5 text-gray-400 hover:text-black dark:text-white" />
 
-<form action="#">
+<form action="#" use:init>
   <div class="space-y-4">
     <Label class="space-y-2">
       <span>Name</span>
-      <Input name="title" class="border font-normal outline-none" placeholder="Type product name" required />
+      <Input name="name" class="border font-normal outline-none" placeholder="Type product name" required />
     </Label>
 
     <Label class="space-y-2">
@@ -22,7 +37,7 @@
     </Label>
     <Label class="space-y-2">
       <span>Technology</span>
-      <Select class="border-gray-300 font-normal outline-none">
+      <Select name="technology" class="border-gray-300 font-normal outline-none">
         <option selected>Select category</option>
         <option value="FL">Flowbite</option>
         <option value="RE">React</option>
@@ -32,11 +47,11 @@
     </Label>
     <Label class="space-y-2">
       <span>Description</span>
-      <Textarea rows={4} placeholder="Enter event description here" class="border-gray-300 font-normal outline-none"></Textarea>
+      <Textarea rows={4} name="description" placeholder="Enter event description here" class="border-gray-300 font-normal outline-none"></Textarea>
     </Label>
     <Label class="space-y-2">
       <span>Discount</span>
-      <Select class="border-gray-300 font-normal outline-none">
+      <Select name="discount" class="border-gray-300 font-normal outline-none">
         <option selected>No</option>
         <option value="5">5%</option>
         <option value="10">10%</option>
@@ -48,7 +63,7 @@
     </Label>
 
     <div class="bottom-0 left-0 flex w-full justify-center space-x-4 pb-4 md:absolute md:px-4">
-      <Button type="submit" class="w-full">Add product</Button>
+      <Button type="submit" class="w-full">{Object.keys(data).length ? 'Edit product' : 'Add product'}</Button>
       <Button color="alternative" class="w-full" onclick={() => (hidden = true)}>
         <CloseOutline />
         Cancel
